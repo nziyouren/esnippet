e646b6ade9252996ea315469fa50b1863b74e3bd
libing.chen <libing.chen@907f3344-9b2b-11dd-bde8-492b52a95b86>
2008-10-16 04:10:28 +0000

    esnippet source code

---
+++ snippet_repo/src/main/java/org/mvnsearch/snippet/domain/Category.java
@@ -0,0 +1,148 @@
+package org.mvnsearch.snippet.domain;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.joda.time.DateTime;
+import org.mvnsearch.ridd.domain.RichDomainSupport;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * domain interface for Category
+ */
+@Entity
+@Table(name = "snippet_category")
+public class Category extends RichDomainSupport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "parent_id")
+    private Integer parentId;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "icon")
+    private String icon;
+    @Column(name = "created_at")
+    @Type(type = "org.joda.time.contrib.hibernate.PersistentDateTime")
+    private DateTime createdAt;
+
+    /**
+     * get category id
+     *
+     * @return category id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * set category id
+     *
+     * @param id category id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * get parent category id
+     *
+     * @return parent category id
+     */
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    /**
+     * set parent category id
+     *
+     * @param parentId parent category id
+     */
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    /**
+     * get category name
+     *
+     * @return category name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * set category name
+     *
+     * @param name category name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * get icon for category
+     *
+     * @return icon name
+     */
+    public String getIcon() {
+        return icon;
+    }
+
+    /**
+     * set icon for category
+     *
+     * @param icon icon name
+     */
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    /**
+     * get created timestamp
+     *
+     * @return created timestamp
+     */
+    public DateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    /**
+     * set created timestamp
+     *
+     * @param createdAt created timestamp
+     */
+    public void setCreatedAt(DateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    /**
+     * get all category in current category, recursive not support
+     *
+     * @return snippet list
+     */
+    @SuppressWarnings({"unchecked"})
+    public List<Snippet> findSnippets() {
+        DetachedCriteria criteria = DetachedCriteria.forClass(Snippet.class);
+        criteria.add(Restrictions.eq("categoryId", id));
+        criteria.addOrder(Order.desc("createdAt"));
+        return getHibernateTemplate().findByCriteria(criteria);
+    }
+
+    /**
+     * get children categories
+     *
+     * @return category list
+     */
+    @SuppressWarnings({"unchecked"})
+    public List<Category> getChildrenCategories() {
+        DetachedCriteria criteria = DetachedCriteria.forClass(Category.class);
+        criteria.add(Restrictions.eq("parentId", id));
+        criteria.addOrder(Order.asc("name"));
+        return getHibernateTemplate().findByCriteria(criteria);
+    }
+
+}
