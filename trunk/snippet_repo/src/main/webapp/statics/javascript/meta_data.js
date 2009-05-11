62ba0f07852f60e1192bb711a44768d78bb021e0
libing.chen <libing.chen@907f3344-9b2b-11dd-bde8-492b52a95b86>
2009-05-11 02:04:27 +0000

    meta data

---
+++ trunk/snippet_repo/src/main/webapp/statics/javascript/meta_data.js
@@ -0,0 +1,25 @@
+/**
+ * @class Snippet
+ */
+function Snippet() {
+    /**
+     * snippet Id
+     */
+    this.id = 1;
+    /**
+     * snippet name
+     */
+    this.name = "";
+    /**
+     * language text
+     */
+    this.languageText = "";
+    /**
+     * mnemonic
+     */
+    this.mnemonic = "";
+    /**
+     * author
+     */
+    this.author = "";
+}
