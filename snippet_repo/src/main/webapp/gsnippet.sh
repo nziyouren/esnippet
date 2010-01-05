7758abe883ed3abd6a4692b87354202f24443768
libing.chen <libing.chen@907f3344-9b2b-11dd-bde8-492b52a95b86>
2010-01-05 15:32:45 +0000

    minor update

--- snippet_repo/src/main/webapp/gsnippet.sh
+++ snippet_repo/src/main/webapp/gsnippet.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -s "http://www.mvnsearch.org/snippet/showPart.action?part=script&id=$1" > $2
-echo "File saved: $2"
+#operation gsnippet.sh 222 > demo.vm
+curl -s "http://www.mvnsearch.org/snippet/showPart.action?part=script&id=$1"
